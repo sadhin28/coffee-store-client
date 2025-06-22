285b3fa52e6464e3b7acdb552e4742e186b7d47e
sadhin28 <taosifsadhin113@gmail.com>
2025-06-23 02:43:03 +0600

    add coffee card

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import { useLoaderData } from "react-router-dom";
+import { MdDeleteForever } from "react-icons/md";
 
 const Home = () => {
+    const allcoffee = useLoaderData();
+    // const { category, chef,details,name, photo,taste,_id
+    // }=allcoffee
+    console.log(allcoffee)
     return (
-        <div>
-            <h1>home</h1>
+        <div className="mx-auto">
+            {
+                allcoffee.map(coffee => <div>
+
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure>
+                            <img
+                                src={coffee.photo}
+                                alt={coffee.name} />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                {coffee.name}
+                               
+                            </h2>
+                            <p>{coffee.details}</p>
+                            <div className="card-actions justify-end">
+                                <div className="badge btn hover:bg-blue-600 hover:text-white badge-outline">Update</div>
+                                <div className="badge text-2xl hover:text-red-600 btn badge-outline"><MdDeleteForever /></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,6 +6,7 @@ import Updatecoffee from "../component/Updatecoffee";
 const router = createBrowserRouter([
     {
         path:'/',
+        loader:()=>fetch('http://localhost:5000/coffee'),
         element:<Home></Home>,
        
 
