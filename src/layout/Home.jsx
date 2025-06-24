d10e86d9d4f2c9bd732c83d3a13b742a5958fda7
sadhin28 <taosifsadhin113@gmail.com>
2025-06-24 22:43:53 +0600

    add sweet alert

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,11 +1,50 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { MdDeleteForever } from "react-icons/md";
-
+import Swal from 'sweetalert2';
 const Home = () => {
     const allcoffee = useLoaderData();
     // const { category, chef,details,name, photo,taste,_id
     // }=allcoffee
+
+    /**======Delate Coffe======= **/
     console.log(allcoffee)
+    const handelDelete = _id => {
+        console.log(_id)
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // swalWithBootstrapButtons.fire({
+                //     title: "Deleted!",
+                //     text: "Your file has been deleted.",
+                //     icon: "success"
+                // });
+                console.log('Delate Conform')
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire({
+                    title: "Cancelled",
+                    text: "Your imaginary file is safe :)",
+                    icon: "error"
+                });
+            }
+        });
+    }
     return (
         <div className="mx-auto">
             {
@@ -20,12 +59,12 @@ const Home = () => {
                         <div className="card-body">
                             <h2 className="card-title">
                                 {coffee.name}
-                               
+
                             </h2>
                             <p>{coffee.details}</p>
                             <div className="card-actions justify-end">
                                 <Link to='update-coffee'><div className="badge btn hover:bg-blue-600 hover:text-white badge-outline">Update</div></Link>
-                                <div className="badge text-2xl hover:text-red-600 btn badge-outline"><MdDeleteForever /></div>
+                                <div onClick={() => handelDelete(coffee._id)} className="badge text-2xl hover:text-red-600 btn badge-outline"><MdDeleteForever /></div>
                             </div>
                         </div>
                     </div>
