6053d9476c33edb519f345d25e5426502dfdc753
sadhin28 <taosifsadhin113@gmail.com>
2025-06-24 23:36:17 +0600

    add sweet alert in update coffee page

--- src/component/Updatecoffee.jsx
+++ src/component/Updatecoffee.jsx
@@ -1,23 +1,40 @@
 import { useForm } from 'react-hook-form';
 import { useLoaderData } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Swal from 'sweetalert2';
 const Updatecoffee = () => {
     const coffee = useLoaderData()
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
-     const onSubmit = async (data) =>{
-        
-     }
+    const onSubmit = async (data) => {
+        Swal.fire({
+            title: "Do you want to save the changes?",
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: "Save",
+            denyButtonText: `Don't save`
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                Swal.fire("Saved!", "",
+                     "success");
+                console.log('Update coffee')
+            
+                console.log(data)
+            } else if (result.isDenied) {
+                Swal.fire("Changes are not saved", "", "info");
+            }
+        });
+    }
     return (
-         <div className="min-h-screen flex items-center justify-center bg-[#f4f3f0] px-4 py-8">
+        <div className="min-h-screen flex items-center justify-center bg-[#f4f3f0] px-4 py-8">
             <div className="w-full max-w-4xl bg-white shadow-md rounded-lg p-10 relative">
                 <a href="/" className="absolute top-4 left-4 text-lg font-semibold text-gray-700 hover:underline">
                     ← Back to home
                 </a>
 
                 <h2 className="text-3xl font-bold text-center text-[#374151] mb-2">Update Coffee</h2>
-                
+
 
                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
